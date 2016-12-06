c7294b2c26e3197533a5c71efa2f868f05ef8658
mozartvn <mozartvnn@gmail.com>
2016-12-06 23:31:57 +0700

    p01

--- p01.scala
+++ p01.scala
@@ -1,3 +1,10 @@
+/***
+
+Problem 1
+Add all the natural numbers below one thousand that are multiples of 3 or 5.*
+
+***/
+
 val r = (1 until 1000).view.filter(n => n % 3 == 0 || n % 5 == 0).sum
 
-assert(r == 233168) // 7 ms
+assert(r == 233168) // 7 ms
